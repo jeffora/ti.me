ec9ba4f05c20480dfe39740ae30e4f10ed3a8f79
Jeff Burn <jeff@jeffburn.com>
2013-03-03 16:07:57 +1000

    Initial commit

---
+++ ti.me/Clock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reactive.Linq;
+using ReactiveUI;
+
+namespace ti.me
+{
+    public class Clock : ReactiveObject
+    {
+        public Clock()
+        {
+            Observable.Timer(TimeSpan.FromMilliseconds(100), TimeSpan.FromMilliseconds(100))
+                .Select(_ => DateTime.Now)
+                .ToProperty(this, c => c.Time);
+        }
+
+        ObservableAsPropertyHelper<DateTime> _Time;
+        public DateTime Time
+        {
+            get { return _Time.Value; }
+        }
+    }
+}
